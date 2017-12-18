3faae70df102a40477b1430588b1b6f97c1cc63e
achimDa <dan.achim@servustech.com>
2017-12-18 09:29:14 +0200

    auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,30 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-// import { AuthService } from '../services/auth.service'
-// import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/operator/do';
-// import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/take';
-
-// @Injectable()
-// export class AuthGuard implements CanActivate {
-// 	constructor(private auth: AuthService, private router: Router) { }
-	
-// 	canActivate(
-// 		next: ActivatedRouteSnapshot,
-// 		state: RouterStateSnapshot): Observable<boolean> | boolean {
-// 		return this.auth.user$
-// 			.take(1)
-// 			.map(user => !!user)
-// 			.do(loggedIn => {
-// 				if (!loggedIn) {
-// 					console.log('access denied')
-// 					this.router.navigate(['/login']);
-// 				}
-// 			})
-// 	}
-// }
-
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
@@ -34,21 +7,21 @@ import { tap, map, take } from 'rxjs/operators';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private auth: AuthService) {}
+	constructor(private auth: AuthService) { }
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> {
+	canActivate(
+		next: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot): Observable<boolean> {
 
-    return this.auth.user$.pipe(
-      take(1),
-      map(user => user && user.roles.admin ? true : false),
-      tap(isAdmin => {
-        if (!isAdmin) {
-          console.error('Access denied - Admins only')
-        }
-      })
-    );
+		return this.auth.user$.pipe(
+			take(1),
+			map(user => user && user.roles.admin ? true : false),
+			tap(isAdmin => {
+				if (!isAdmin) {
+					console.error('Access denied - Admins only')
+				}
+			})
+		);
 
-  }
+	}
 }
