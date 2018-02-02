8c170c50646da32bf4995e230e9cdeb39510c03f
achimDa <dan.achim@servustech.com>
2018-02-02 16:52:25 +0200

    nav changes

--- src/app/home/nav/nav.component.ts
+++ src/app/home/nav/nav.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger ,style, transition, animate, keyframes, query, stagger, state } from '@angular/animations';
 import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-nav',
 	templateUrl: './nav.component.html',
 	styleUrls: ['./nav.component.less'],
+	animations: [
+		trigger('slideInOut', [
+			state('in', style({ height: '0px', display: 'none', opacity: '0' })),
+			state('out', style({ height: '*'})),
+			transition('in => out', animate('200ms ease-in')),
+			transition('out => in', animate('200ms ease-out'))
+		])
+	]
 })
 export class NavComponent implements OnInit {
-
+	
+	@Input() isVisible : boolean = true;
+	
 	navigation: Array<{}>
 	user: any
 
 	isVisibile:boolean = false
-	isToggle:boolean = false
+	isToggle: string
 		
 	constructor(private router: Router) {}
 	
@@ -33,20 +44,24 @@ export class NavComponent implements OnInit {
 			}
 		];
 
-		if (window.innerWidth <= 768) this.isVisibile = true
+		if (window.innerWidth <= 768) {
+			this.isVisibile = true
+			this.isToggle = 'in'
+		} 
 	}
 
 	onResize(event:any) {
 		if (event.target.innerWidth <= 768) {
 			this.isVisibile = true
+			this.isToggle = 'in'
 		} else {
 			this.isVisibile = false
+			this.isToggle = 'out'
 		}
-		console.log(event.target.innerWidth)
 	}
 
 	toggleMenu() {
-		this.isToggle =! this.isToggle
+		if (window.innerWidth <= 768) this.isToggle = this.isToggle === 'out' ? 'in' : 'out'
 	}
 
 	public navigate(button: any) {
