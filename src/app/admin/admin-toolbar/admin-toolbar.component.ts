70baf3db9b77e7d00d829b54df41fcdb7f83160c
achimDa <dan.achim@servustech.com>
2018-02-15 12:47:10 +0200

    admin user photo

--- src/app/admin/admin-toolbar/admin-toolbar.component.ts
+++ src/app/admin/admin-toolbar/admin-toolbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'admin-toolbar',
-  templateUrl: './admin-toolbar.component.html',
-  styleUrls: ['./admin-toolbar.component.less']
+	selector: 'admin-toolbar',
+	templateUrl: './admin-toolbar.component.html',
+	styleUrls: ['./admin-toolbar.component.less']
 })
 export class AdminToolbarComponent implements OnInit {
 
-  constructor() { }
+	userPhoto: string = '';
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+
+		var user = firebase.auth().currentUser.photoURL;
+		if (user) {
+			this.userPhoto = user;
+		} else {
+			this.userPhoto = '/assets/imgs/man.png';
+		}
+	}
 
 }
